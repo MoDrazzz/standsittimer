88e7f97755b3b0f49aa5ddad292fa8b16779312e
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2025-09-08 09:50:53 +0200

    feat: show remaining time

--- StandSitTimerApp.swift
+++ StandSitTimerApp.swift
@@ -6,24 +6,34 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var isStanding = true
     var lastChange = Date()
     
-    let standDuration: TimeInterval = 30 * 60 // 30 minutes
-    let sitDuration: TimeInterval = 60 * 60  // 60 minutes
+    let standDuration: TimeInterval = 30 * 60 // 30 min
+    let sitDuration: TimeInterval = 60 * 60  // 60 min
+    
+    // Menu items
+    let timeRemainingItem = NSMenuItem(title: "Time Remaining: --:--", action: nil, keyEquivalent: "")
+    let quitItem = NSMenuItem(title: "Quit", action: #selector(quitApp), keyEquivalent: "q")
     
     func applicationDidFinishLaunching(_ notification: Notification) {
+        // Menu bar icon
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-        
         updateIcon()
-        startTimer()
         
+        // Menu
         let menu = NSMenu()
-        menu.addItem(NSMenuItem(title: "Quit", action: #selector(quitApp), keyEquivalent: "q"))
+        menu.addItem(timeRemainingItem)
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(quitItem)
         statusItem.menu = menu
+        
+        startTimer()
     }
     
     func startTimer() {
         timer = Timer.scheduledTimer(withTimeInterval: 60, repeats: true) { _ in
             self.checkState()
+            self.updateTimeRemaining()
         }
+        updateTimeRemaining() // update immediately on launch
     }
     
     func checkState() {
@@ -46,6 +56,19 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                 button.image = NSImage(systemSymbolName: "chair", accessibilityDescription: "Sit")
             }
         }
+    }
+    
+    func updateTimeRemaining() {
+        let now = Date()
+        let elapsed = now.timeIntervalSince(lastChange)
+        let duration = isStanding ? standDuration : sitDuration
+        let remaining = max(0, Int(duration - elapsed))
+        
+        let minutes = remaining / 60
+        let seconds = remaining % 60
+        let phase = isStanding ? "Standing" : "Sitting"
+        
+        timeRemainingItem.title = "Time Remaining (\(phase)): \(String(format: "%02d:%02d", minutes, seconds))"
     }
     
     @objc func quitApp() {
